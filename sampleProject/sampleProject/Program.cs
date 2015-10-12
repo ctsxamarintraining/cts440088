b1cd0b4db460ea9a9f0f71e2994e9130274d8fb9
CTS440088 <sravanvarma03@gmail.com>
2015-10-12 17:39:34 +0530

    Exceptions-Task 1

---
+++ sampleProject/sampleProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sampleProject
+{
+	class MainClass
+	{
+		static int k = 0;
+		public static void Main (string[] args)
+		{
+			int[] sampleArray=new int[10];
+
+			do{
+			Console.WriteLine("enter number:");
+			string readData=Console.ReadLine();
+			try{
+
+				int i=Int32.Parse(readData);
+				sampleArray[k]=i;
+				k++;  
+								for(int a=0;a<k;a++)
+								{
+						Console.Write("{0},",sampleArray[a]);
+								}
+					Console.WriteLine();
+			}
+			catch(IndexOutOfRangeException e) { 
+				Console.WriteLine (e.Message);
+			}
+			catch(FormatException e) {
+				Console.WriteLine (e.Message);
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+			finally
+			{
+
+			}
+			}while(true);
+		}
+
+	}
+}
